97aa26a9a3e24d30c28243ecfd1e2acae74274ea
Ahire Hitesh <hbahire2014@gmail.com>
2021-07-03 08:26:40 +0530

    Pattern and Sort code has been done

---
+++ String_Opr/src/Pallindrome.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Pallindrome {
+
+	public boolean isPallindrome(String str) {
+
+		for (int i = 0; i < str.length() / 2; i++) {
+			if(str.charAt(i) == str.charAt(str.length() - (i + 1)))
+				continue;
+			else
+				return false;
+		}
+		return true;
+	}
+
+	public static void main(String[] argv) {
+
+		Pallindrome obj = new Pallindrome();
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter String : ");
+		String str = sc.next();
+
+		if ( obj.isPallindrome(str))
+			System.out.println("Pallindrome");
+		else 
+			System.out.println("Not pallindrome.");
+	}
+}
